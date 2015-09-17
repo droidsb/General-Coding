029c1dd2562f7afa6d6e8ea401ad17a95b049e38
Seth Baird <SethBaird@Seths-Macbook.attlocal.net>
2015-09-16 17:46:44 -0700

    hello

---
+++ class_1/project3.js
@@ -0,0 +1,18 @@
+
+var x = Math.ceil(Math.random() * 10);
+var y = Math.ceil(Math.random() * 10);
+
+console.log('x='+x);
+console.log('y='+y);
+
+
+	if(x<y){
+	console.log('x is less than y')
+	}
+	
+	else if(x>y){
+	console.log('x is greater than y');
+	}
+	else if(x===y){
+	console.log('x is equal to y')
+	}
