ed8ec1ed16364efafb53d3939dc2cb91177cc14c
Seth Baird <SethBaird@Seths-Macbook.attlocal.net>
2015-09-16 19:41:49 -0700

    Added !=, <=, and >=

--- class_1/project3.js
+++ class_1/project3.js
@@ -1,18 +1,25 @@
 
 var x = Math.ceil(Math.random() * 10);
 var y = Math.ceil(Math.random() * 10);
-
-console.log('x='+x);
-console.log('y='+y);
-
-
-	if(x<y){
-	console.log('x<y')
-	}
+if(x>=y || x<=y){
+	if(x!=y){	
+	console.log('The numbers are not equal');
 	
-	else if(x>y){
-	console.log('x>y');
+	
+		if(x<y){
+		console.log(x+'<'+y);
+		}
+	
+		else if(x>y){
+		console.log(x+'>'+y);
+		}
 	}
-	else if(x===y){
-	console.log('x=y')
-	}
+
+		else if(x===y){
+		console.log('The numbers are equal');
+		console.log(x+'='+y);
+		
+		
+		}
+	
+}	
