beeffadf6395e9cd7984f7c3175d1035ce7a4776
Seth Baird <SethBaird@Seths-Macbook.attlocal.net>
2015-09-18 16:16:13 -0700

    added project6.js

---
+++ class_1/project6.js
@@ -0,0 +1,99 @@
+
+	//var cardnum = Math.ceil(Math.random() * 52);
+	//var cardsuit = Math.ceil(Math.random() * 4);
+	
+	for (var cardnum = 1; cardnum<=13; cardnum=cardnum+1){
+	
+	if(cardnum===1){
+	console.log('');
+	console.log('SUIT 1');
+console.log('Ace of Spades');	
+	}
+	else if(cardnum>1 && cardnum<10){
+	console.log(cardnum+' of Spades');
+	}
+	else if(cardnum===11){
+	console.log('Jack of Spades');
+	}
+	else if(cardnum===12){
+	console.log('Queen of Spades');
+	}
+	else if(cardnum===13){
+	console.log('King of Spades');
+	}
+	}
+	
+	
+	
+	
+	for (var cardnum = 1; cardnum<=13; cardnum=cardnum+1){
+	
+	if(cardnum===1){
+	console.log('');
+	console.log('SUIT 2');
+console.log('Ace of Diamonds');	
+	}
+	else if(cardnum>1 && cardnum<=10){
+	console.log(cardnum+' of Diamonds');
+	}
+	else if(cardnum===11){
+	console.log('Jack of Diamonds');
+	}
+	else if(cardnum===12){
+	console.log('Queen of Diamonds');
+	}
+	else if(cardnum===13){
+	console.log('King of Diamonds');
+	}
+	}
+	
+	
+	
+	
+	for (var cardnum = 1; cardnum<=13; cardnum=cardnum+1){
+	
+	if(cardnum===1){
+	console.log('');
+	console.log('SUIT 3');
+console.log('Ace of Hearts');	
+	}
+	else if(cardnum>1 && cardnum<=10){
+	console.log(cardnum+' of Hearts');
+	}
+	else if(cardnum===11){
+	console.log('Jack of Hearts');
+	}
+	else if(cardnum===12){
+	console.log('Queen of Hearts');
+	}
+	else if(cardnum===13){
+	console.log('King of Hearts');
+	}
+	}
+	
+
+	
+	
+	for (var cardnum = 1; cardnum<=13; cardnum=cardnum+1){
+	
+	if(cardnum===1){
+	console.log('');
+	console.log('SUIT 4');
+console.log('Ace of Clubs');	
+	}
+	else if(cardnum>1 && cardnum<=10){
+	console.log(cardnum+' of Clubs');
+	}
+	else if(cardnum===11){
+	console.log('Jack of Clubs');
+	}
+	else if(cardnum===12){
+	console.log('Queen of Clubs');
+	}
+	else if(cardnum===13){
+	console.log('King of Clubs');
+	}
+	
+	}
+	//this isn't working right now :( V
+	//console.log(cardnum+' cards');
