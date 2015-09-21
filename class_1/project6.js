25b44b74adffd66c8f43bc9272e8f74112db365e
Seth Baird <SethBaird@Seths-Macbook.attlocal.net>
2015-09-21 16:43:58 -0700

    Even more simple

--- class_1/project6.js
+++ class_1/project6.js
@@ -4,39 +4,37 @@
 var suit = 'Suit'; 
 var suitchanger = 1;	
 
+
 for (var cardnum = 1; cardnum<=13; cardnum=cardnum+1){
 if(suitchanger===1){
 suit='Spades'
+
 }
 if(suitchanger===2){
 suit='Diamonds'
+
 }
 if(suitchanger===3){
 suit='Clubs'
+
 }
 if(suitchanger===4){
 suit='Hearts'
+
 }
 	if(cardnum<=4){
 	console.log('');
 console.log('Ace of '+suit);	
 
+for (var CC=2; CC<=10; CC=CC+1){
+console.log(CC+' of '+suit);
 
-console.log('2 of '+suit);
-console.log('3 of '+suit);
-console.log('4 of '+suit);
-console.log('5 of '+suit);
-console.log('6 of '+suit);
-console.log('7 of '+suit);
-console.log('8 of '+suit);
-console.log('9 of '+suit);
-console.log('10 of '+suit);
-
-	
+	}
 	
 console.log('Jack of '+suit);
 console.log('Queen of '+suit);
 console.log('King of '+suit);
+suitchanger=suitchanger+1
 }
-suitchanger=suitchanger+1;
+
 }
