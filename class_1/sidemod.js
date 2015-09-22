59abc005b826d1d21420a5c4f1330cf7527dd026
Seth Baird <SethBaird@Seths-Macbook.attlocal.net>
2015-09-22 09:40:26 -0700

    simple

--- class_1/sidemod.js
+++ class_1/sidemod.js
@@ -2,7 +2,7 @@ var fish = Math.ceil(Math.random() * 1000);
 var car = Math.ceil(Math.random() * 6);
 var ani =1;
 var number=1;
-
+var FC=car*fish;
 
 
 console.log(fish); 
@@ -15,29 +15,29 @@ console.log(fish%2);
 	console.log('there are bugs in the program');
 	}
 
-	if(car*fish%5===0){
+	if(FC%5===0){
 	console.log('success');
 
 	}
-	if(car*fish%5===1){
+	if(FC%5===1){
 	console.log('CRACK!');
 
 	}
-	if(car*fish%5===2){
+	if(FC%5===2){
 	console.log('BOOM!');
 
 	}
-	if(car*fish%5===3){
+	if(FC%5===3){
 	console.log('Sizzle');
 
 	}
-	if(car*fish%5===4){
+	if(FC%5===4){
 	console.log('SNAP!');
 
 	}
-	if(car*fish%5===0 && car*fish%10===0 ){
+	if(FC%5===0 && FC%10===0 ){
 	console.log('BOOM CRASH');
 
 	}
-	console.log('%5:'+car*fish%5+' %10:'+car*fish%10);
-	console.log(car*fish%5);
+	console.log('%5:'+FC%5+' %10:'+FC%10);
+	console.log(FC%5);
