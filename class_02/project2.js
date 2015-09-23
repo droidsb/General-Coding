0144fe49871adf937a118e5b6bb98c465d0da8db
Seth <capnsethb@gmail.com>
2015-09-23 16:59:36 -0700

    call
    whats calling the addresses?

--- class_02/project2.js
+++ class_02/project2.js
@@ -1,29 +1,39 @@
-var address={
+
+var BobA={
 "state" : "CA",
 "name" : "Bob Jones",
 "street" : "123 any street",
 "zip_code" : 54321,
 "city" : "Anytown"
 };
-var address2={
+var ChuckA={
 "state" : "CA",
 "name" : "Chuck Noris",
 "street" : "123 Awesome Ville",
-"zip_code" : 42,
+"zip_code" : 42420,
 "city" : "Cloud Surface"
 };
+var PeopleAddress={
+"Chuck" : ChucksAddress(ChuckA),
+"Bob" : BobsAddress(BobA)
+
+
+}
 
 
 //console.log(Object.keys(Dictionary));
-function BobsAddress(address){
-	console.log(address['name']);
-	console.log(address['street']);
-	console.log(address['city']+' '+address['state']+'	'+address['zip_code']);
+function BobsAddress(BobA){
+	console.log(BobA['name']);
+	console.log(BobA['street']);
+	console.log(BobA['city']+' '+BobA['state']+'	'+BobA['zip_code']);
 }
-var PA=0;
-if(PA=0){
+function ChucksAddress(ChuckA){
+	console.log(ChuckA['name']);
+	console.log(ChuckA['street']);
+	console.log(ChuckA['city']+' '+ChuckA['state']+'	'+ChuckA['zip_code']);
+}
 
-}
-BobsAddress(address);
+
+//console.log(PeopleAddress['Bob']);
 
 
