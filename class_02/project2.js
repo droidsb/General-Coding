adda059d4a53c0e32f9944e2c81257d2c4a0f92c
Seth <capnsethb@gmail.com>
2015-09-23 16:46:53 -0700

    Fixes #11
    deleted return and added call

--- class_02/project2.js
+++ class_02/project2.js
@@ -5,16 +5,25 @@ var address={
 "zip_code" : 54321,
 "city" : "Anytown"
 };
+var address2={
+"state" : "CA",
+"name" : "Chuck Noris",
+"street" : "123 Awesome Ville",
+"zip_code" : 42,
+"city" : "Cloud Surface"
+};
 
 
 //console.log(Object.keys(Dictionary));
-function BobsAddress(){
+function BobsAddress(address){
 	console.log(address['name']);
 	console.log(address['street']);
 	console.log(address['city']+' '+address['state']+'	'+address['zip_code']);
 }
+var PA=0;
+if(PA=0){
 
-//if(PA=0){
-return BobsAddress;
-//}
+}
+BobsAddress(address);
+
 
