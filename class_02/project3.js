68e060a27c484957787b158c5bae9b4382a4fab4
Seth <capnsethb@gmail.com>
2015-09-24 12:02:27 -0700

    project3.js
    this project is finished(may have bugs)
    fixed #13

--- class_02/project3.js
+++ class_02/project3.js
@@ -1,13 +1,18 @@
 function print_equation(x1, y1, x2, y2){
+	if(x2===x1){
+		console.log('x = '+x1)
+		return;
+	}
 
 	var m = (y2-y1)/(x2-x1);
 	var mx = m*x1;
 	var b = y1-mx;
 	var y = mx+b;
-    console.log(m);
-    console.log(b);
-	console.log(y);
-
+    console.log('m='+m);
+    console.log('b='+b);
+	console.log('y='+y);
+	console.log('y='+m+'x+'+b)
+	
 }
 
-	print_equation(1,3,3,7);
+	print_equation(1,2,4,2);
