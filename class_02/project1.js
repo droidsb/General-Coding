856710cd4e79049d842c1b2f564fd38cd1ad42fb
Seth <capnsethb@gmail.com>
2015-09-24 08:13:17 -0700

    project one
    cleaned up and made more simple project 1 class 2
    fixed #10

--- class_02/project1.js
+++ class_02/project1.js
@@ -1,6 +1,4 @@
-var MyNum = 3;
-console.log(MyNum);
-	function MyNumF(){
-	return MyNum * 2;
+function MyNumFun(MyArg){
+	return MyArg * 2;
 }
-console.log(MyNumF(MyNum));	
+	console.log(MyNumFun(42));	
