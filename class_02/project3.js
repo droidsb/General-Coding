6663526710c950474dc01f0e675c7d9b4198a674
Seth <capnsethb@gmail.com>
2015-09-24 11:37:30 -0700

    update
    edited file slightly

--- class_02/project3.js
+++ class_02/project3.js
@@ -1,9 +1,10 @@
 function print_equation(x1, y1, x2, y2){
-	var m = y2-y1/x2-x1;
+	var m = (y2-y1)/(x2-x1);
 
 	var b = y1-m*x1;
 	var y = m*x1+b;
-
+    console.log(m);
+    console.log(b);
 	console.log(y);
 
 }
