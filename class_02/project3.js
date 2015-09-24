58921e41104d892c40686f75183bb0edb6da017c
Seth <capnsethb@gmail.com>
2015-09-24 11:40:08 -0700

    small
    another small change…
    just better equations

--- class_02/project3.js
+++ class_02/project3.js
@@ -1,8 +1,9 @@
 function print_equation(x1, y1, x2, y2){
-	var m = (y2-y1)/(x2-x1);
 
-	var b = y1-m*x1;
-	var y = m*x1+b;
+	var m = (y2-y1)/(x2-x1);
+	var mx = m*x1;
+	var b = y1-mx;
+	var y = mx+b;
     console.log(m);
     console.log(b);
 	console.log(y);
