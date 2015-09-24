200ceffad5180ba73eb17425a5e24bdd20643c6d
Seth <capnsethb@gmail.com>
2015-09-24 10:31:50 -0700

    finished project2.js
    may need more work but you never know

--- class_02/project2.js
+++ class_02/project2.js
@@ -19,20 +19,7 @@ var God={
 	"zip_code" : 7777777,
 	"city" : "The City of God:"
 }
-
-/*var PeopleAddress={
-	"Chuck" : PrintAddress(ChuckA),
-	"Bob" : PrintAddress(BobA)
-
-
-}*/
-
-
-
-
-
-
-  console.log('List:Bob_Jones, Chuck_Noris, God');
+console.log('List:Bob_Jones, Chuck_Noris, God');
   console.log('');
  
 function PrintAddress(Add){
@@ -40,19 +27,12 @@ function PrintAddress(Add){
 	console.log(Add['street']);
 	console.log(Add['city']+' '+Add['state']+'	'+Add['zip_code']);
 }
-/*function PrintAddress(Chuck_Noris){
-	console.log(Chuck_Noris['name']);
-	console.log(Chuck_Noris['street']);
-	console.log(Chuck_Noris['city']+' '+Chuck_Noris['state']+'	'+Chuck_Noris['zip_code']);
-}
-function PrintAddress(God){
-	console.log(God['name']);
-	console.log(God['street']);
-	console.log(God['city']+' '+God['state']+'	'+God['zip_code']);
-}*/
-
 //Change this __V__ to get their addresses by type their first and last name
   PrintAddress(Chuck_Noris);
+  console.log('');
+  PrintAddress(Bob_Jones);
+  console.log('');
+  PrintAddress(God);
  //Example:Bob_Jones
 
 
