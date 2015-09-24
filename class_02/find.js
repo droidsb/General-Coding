fafd6f380b0fce59eacd8848e86814c32c64da9f
Seth <capnsethb@gmail.com>
2015-09-24 14:08:16 -0700

    fixed function
    made it so that it is still a black box!

--- class_02/find.js
+++ class_02/find.js
@@ -1,28 +1,27 @@
 //this finds the miles traveled at the speed and time given
-for (x = 0; x <= 10; x = x + 1) {
-    var hours = Math.ceil(Math.random() * 8);
-    var speed = Math.floor(Math.random() * 160) - 80;
-    var MyArg = speed * hours;
+function distance(MyArg) {
+    if (hours < 1) {
+        console.log('The hours can not be below or equal to zero');
+        return;
+    }
 
-    function distance(MyArg) {
-        if (hours < 1) {
-            console.log('The hours can not be below or equal to zero');
-            return;
-        }
-
-        if (speed * hours > 0) {
-            return MyArg;
-        }
-        if (speed * hours < 0) {
-            return MyArg * -1;
-
-        }
+    if (speed * hours > 0) {
+        return MyArg;
+    }
+    if (speed * hours < 0) {
+        return -MyArg;
 
     }
 
+}
+for (x = 0; x <= 10; x = x + 1) {
+    var hours = Math.ceil(Math.random() * 8);
+    var speed = Math.floor(Math.random() * 160) - 90;
+   
+
     console.log('hours:' + hours);
-    console.log('MPH:' + speed);
-    console.log(distance(MyArg) + ' miles traveled');
+    console.log('speed:' + speed);
+    console.log(distance(speed*hours) + ' miles traveled');
     console.log('');
 
 
