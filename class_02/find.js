d177648e64e0158a43db8402e4710678c82e50d1
Seth <capnsethb@gmail.com>
2015-09-24 14:28:20 -0700

    bugs
    bugs, bugs, bugs, and more bugs
    but they are fixed :)

--- class_02/find.js
+++ class_02/find.js
@@ -1,11 +1,8 @@
 //this finds the miles traveled at the speed and time given
 function distance(MyArg) {
-    if (MyArg < 1) {
-        console.log('The hours or speed can not be below or equal to zero');
-        return;
-    }
+   
 
-    if (MyArg > 0) {
+    if (MyArg >= 0) {
         return MyArg;
     }
     if (MyArg < 0) {
@@ -16,7 +13,7 @@ function distance(MyArg) {
 }
 for (x = 0; x <= 10; x = x + 1) {
     var hours = Math.ceil(Math.random() * 8);
-    var speed = 0;//Math.floor(Math.random() * 160) - 80;
+    var speed = Math.floor(Math.random() * 160) - 80;
    
 
     console.log('hours:' + hours);
