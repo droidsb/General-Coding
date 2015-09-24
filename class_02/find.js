2d22747ca39acaa52fdfea3f9d5740b71985c6a0
Seth <capnsethb@gmail.com>
2015-09-24 13:57:16 -0700

    FORMAT
    It is very important

--- class_02/find.js
+++ class_02/find.js
@@ -1,28 +1,29 @@
 //this finds the miles traveled at the speed and time given
-for(x=0; x<=10; x=x+1){
-var hours=Math.ceil(Math.random() * 8);
-var speed=Math.floor(Math.random() * 160)-80;
-var MyArg = speed*hours;
-function distance(MyArg){
-if(hours<1){
-console.log('The hours can not be below or equal to zero');
-return;
-}
+for (x = 0; x <= 10; x = x + 1) {
+    var hours = Math.ceil(Math.random() * 8);
+    var speed = Math.floor(Math.random() * 160) - 80;
+    var MyArg = speed * hours;
 
-if(speed*hours>0){
-return MyArg;
-}
-if(speed*hours<0){
-return MyArg*-1;
+    function distance(MyArg) {
+        if (hours < 1) {
+            console.log('The hours can not be below or equal to zero');
+            return;
+        }
 
-}
+        if (speed * hours > 0) {
+            return MyArg;
+        }
+        if (speed * hours < 0) {
+            return MyArg * -1;
 
-}
+        }
 
-console.log('hours:'+hours);
-console.log('MPH:'+speed);
-console.log(distance(MyArg)+' miles traveled');
-console.log('');
+    }
+
+    console.log('hours:' + hours);
+    console.log('MPH:' + speed);
+    console.log(distance(MyArg) + ' miles traveled');
+    console.log('');
 
 
 }
