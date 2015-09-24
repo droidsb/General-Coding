0c8262e4081eae7f5b7d46e1e5978702e0c8806a
Seth <capnsethb@gmail.com>
2015-09-24 15:10:17 -0700

    name change

---
+++ class_02/distance.js
@@ -0,0 +1,23 @@
+//this finds the miles traveled at the speed and time given
+function absolute(MyArg) {
+   if (MyArg >= 0) {
+        return MyArg;
+    }
+    if (MyArg < 0) {
+        return -MyArg;
+
+    }
+
+}
+for (x = 0; x <= 10; x = x + 1) {
+    var hours = Math.ceil(Math.random() * 8);
+    var speed = Math.floor(Math.random() * 160) - 80;
+   
+
+    console.log('hours:' + hours);
+    console.log('speed:' + speed);
+    console.log(absolute(speed*hours) + ' miles traveled');
+    console.log('');
+
+
+}
