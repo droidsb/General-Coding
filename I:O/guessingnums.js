2df2a5dd63ce1f239df5bcdff939e197dde7651b
Seth <capnsethb@gmail.com>
2015-10-07 16:01:07 -0700

    guessing nums
    added limited time and finished project, there may be some bugs

--- I:O/guessingnums.js
+++ I:O/guessingnums.js
@@ -1,24 +1,53 @@
 var readline = require("readline-sync");
-
+var x=10;
 var MyRN = Math.ceil(Math.random() * 100);
-console.log(MyRN);
-var guess1 = readline.question("Guess the number I am thinking of from 1-100 ");
 
-if(guess1 === MyRN){
+var guess1 = readline.question("Guess the number I am thinking of from 1-100 ");
+var guessNum = Number(guess1)
+if(guessNum === MyRN){
 console.log("CORRECT! The number was "+MyRN);
 }
 
-/*if(guess1 != RN){
+if(guessNum != MyRN){
 console.log("Incorrect");
 guessmain();
 }
+
 function guessmain(){
-	if(guess != RN){
-		console.log("Incorrect");
-		var guess = readline.question("try again ");
+	var guessmainvar = readline.question("try again ");
+	var guessNum = Number(guessmainvar)
+	if(x===0){
+	console.log("You ran out of guesses!");
+	setTimeout(function(){}, 1000);
+	console.log("Quitting game...");
+	setTimeout(function(){}, 2000);
+	return;
 	}
-	if(guess === RN){
-		console.log("CORRECT! The number was "+RN);
+	if(guessmainvar === 'q'){
+		console.log("Quitting game...");
+		setTimeout(function(){}, 2000);
+	}
+	else if(guessNum === MyRN){
+		console.log("CORRECT! The number was "+MyRN);
+		
+	}
+	else if(guessNum > MyRN){
+		console.log("Incorrect");
+		console.log("Your answer is too high");
+		x=x-1;
+		console.log(x+" guesses left");
+		guessmain();
+	}
+	else if(guessNum < MyRN){
+		console.log("Incorrect");
+		console.log("Your answer is too low");
+		x=x-1;
+		console.log(x+" guesses left");
+		guessmain();
 	
 	}
-}*/
+	
+	
+	
+}
+
