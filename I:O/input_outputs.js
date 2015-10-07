3bb1bd468a950d712e6aaa2c7ed27a237764a6d6
Seth <capnsethb@gmail.com>
2015-10-07 13:40:31 -0700

    The doctor
    He fixes some of your problems!

--- I:O/input_outputs.js
+++ I:O/input_outputs.js
@@ -3,11 +3,42 @@ var readline = require("readline-sync");
 
 var answer = readline.question("What's your name? ");
 
-console.log("I knew that, "+ answer + "!");
+console.log("Well then! Welcome to my office "+ answer + "!");
 
+setTimeout(QF2,2000)
 
-var count = readline.question("Give me a number: ");
- 
-for (var i = 0; i < count; i = i +1){
-	console.log(i+1);
+function QF2(){
+	
+	var Q2 = readline.question("What seems to be the problem? ");
+	setTimeout(QF3,2000)
 }
+
+function QF3(){
+	
+	console.log('Ah, I see...');
+	setTimeout(how,2000)
+	
+	function how(){
+		var Q3 = readline.question("How does it make you feel? ");
+		var pre = 'My prescription for feeling ' +Q3+ ' is, ';
+		console.log('Hmmmm...');
+		if(Q3==='sad' || Q3==='Sad' || Q3==='Very sad' || Q3==='very sad' ){
+		pre=pre+'go hang out with some friends or enjoy a good joke.'
+		}
+		else if(Q3==='Hungry' || Q3==='hungry' || Q3==='Very hungry' || Q3==='very hungry'){
+		pre=pre+'to go to the store and buy some more food.';
+		}
+		else if(Q3!='sad' || Q3!='Sad' || Q3!='Very sad' || Q3!='very sad' || Q3!='Hungry' || Q3!='hungry'){
+		pre=pre+"to go and ask a real doctor for help. I don't have that problem in my programming yet.";
+		}
+		setTimeout(AF4,2000)
+   
+   function AF4(){
+		
+console.log(pre);
+	}
+
+    }
+}
+
+
