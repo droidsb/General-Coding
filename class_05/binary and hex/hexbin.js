06003fcd21b9f846a16bb12933727ba7811b2788
Seth <capnsethb@gmail.com>
2015-10-15 10:12:15 -0700

    finishing touches

--- class_05/binary and hex/hexbin.js
+++ class_05/binary and hex/hexbin.js
@@ -1,5 +1,5 @@
-//var readline = require("readline-sync");
-//var hexnumI = readline.question("Type a decimal to convert to hexadecimal ");
+var readline = require("readline-sync");
+var hexnumI = readline.question("Type a decimal to convert to hexadecimal ");
 function hexabin(dec){
 if(dec>65535){
 console.log("No numbers bigger than 65,535 are supported");
@@ -7,10 +7,6 @@ return;
 }
 
 var S = [Math.floor(dec%16), Math.floor((dec/16)%16), Math.floor((dec/16/16)%16), Math.floor((dec/16/16/16)%16)]
-var S1 = Math.floor(dec%16);
-var S2 = Math.floor((dec/16)%16);
-var S3 = Math.floor((dec/16/16)%16);
-var S4 = Math.floor((dec/16/16/16)%16);
 for (i=0; i<=3; i++){
 if (S[i]===10){
 S[i]="A"
@@ -32,11 +28,11 @@ S[i]="F"
 
 }
 }
-
-console.log(S[3]+S[2]+S[1]+S[0]);
+console.log(S);
+console.log(S[3]+""+S[2]+""+S[1]+""+S[0]);
 
 }
 //var dec = Number(hexnumI);
 //for (i=0; i<=180; i++){
-hexabin(65535);
+hexabin(hexnumI);
 //}
