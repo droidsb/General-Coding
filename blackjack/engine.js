6b2104c0cac161b8168a0531c3bdd505fbe2b55e
Seth <capnsethb@gmail.com>
2015-11-18 11:11:55 -0800

    Built out engine to work with AI and Players

--- blackjack/engine.js
+++ blackjack/engine.js
@@ -6,6 +6,9 @@ var engine = function(){};
 /**************************************************************************************/
 // module variables
 var _deck = [];
+var _HandArray = [];
+var _PlayerCount = 0;
+var _CurrentPlayer = 0;
 
 /**************************************************************************************/
 // private routines
@@ -88,7 +91,7 @@ var _prettyHand = function (array) {
 // pubic routines
 
 /**************************************************************************************/
-engine.prototype.init = function(){
+engine.prototype.init = function(PlayerAmount){
 	
 
 
@@ -98,16 +101,56 @@ engine.prototype.init = function(){
 	
 	_shuffleArray(_deck);
 	
-	
-	
-	var p1h = _newHand(_deck);
-	var p2h = _newHand(_deck);
-	console.log(p1h);
-	console.log(p2h);
-	console.log(_prettyHand(p1h));
-	console.log(_prettyHand(p2h));
+	for(var i=0; i<PlayerAmount; i++){
+		_HandArray.push(_newHand(_deck));
+		
+	}
+	_PlayerCount = PlayerAmount;
+	console.log(_HandArray);
 };
 
+engine.prototype.cp = function(){
+	return _CurrentPlayer;
+}
+engine.prototype.ch = function(){
+	return _HandArray[_CurrentPlayer];
+}
+engine.prototype.done = function(){
+	return _CurrentPlayer===_PlayerCount;
+}
+engine.prototype.action = function(ActionS){
+	if(ActionS === "hit"){
+		console.log("You got a card!");
+		this.ch().push(_topCard(_deck));
+	}
+	else if(ActionS === "stay"){
+		console.log("Awwww... You didn't get a card, how boring");
+		++_CurrentPlayer;
+	}
+}
+engine.prototype.chv = function(){
+	//1-9 is = value + 1
+	var CurrentHand = this.ch();
+	var result = 0;
+	for(i=0; i<CurrentHand.length; ++i){
+		var CurrentCard = CurrentHand[i]%13;
+		if(CurrentCard===0){
+			result +=11;	
+		}
+		else if(CurrentCard>0 && CurrentCard<10){
+			result +=CurrentCard+1;	
+		}
+		else{
+			result +=10;
+		}
+	
+	}
+	
+	return result;
+}
+engine.prototype.cph = function(){
+	return _prettyHand(this.ch());
+}
 /**************************************************************************************/
 // module suffix
 module.exports = new engine();
--- blackjack/main.js
+++ blackjack/main.js
@@ -1,3 +1,17 @@
 var engine = require('./engine');
 
-engine.init();
+engine.init(3);
+
+while(!engine.done()){
+	
+	console.log("It is now the turn of player "+engine.cp());
+	console.log("Their hand is "+engine.cph());
+	console.log("Their hand value is "+engine.chv());
+	var choice = "hit";
+	if(engine.chv()>=18){
+		choice = "stay";
+	}
+	engine.action(choice);
+	
+}
+console.log("The game has ended!");
