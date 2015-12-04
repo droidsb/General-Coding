75302e244af4e7ac91435ed2f1d685641e361722
Seth <capnsethb@gmail.com>
2015-12-03 20:57:23 -0800

    saved

---
+++ tests/password.js
@@ -0,0 +1,23 @@
+var readline = require('readline-sync');
+
+
+var username = function(){
+var username = readline.question("Username: ");
+
+if(username==="droidsb"){
+	console.log("YOU'RE DA BOSS! :D");
+	return;
+}
+if(username==="jblueskull"){
+	console.log("You're second in command");
+	return;
+}
+else{
+	console.log("You are signed in");
+	return;
+}
+};
+var password = function(){
+var username = readline.question("Password: ");
+
+}:
