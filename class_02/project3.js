23a745527964e5b2ea89773f7985d49919c976cc
Seth <capnsethb@gmail.com>
2015-12-07 16:11:24 -0800

    added some new stuff to project03.js

--- class_02/project3.js
+++ class_02/project3.js
@@ -1,4 +1,5 @@
-function print_equation(x1, y1, x2, y2){
+var readline = require('readline-sync');
+function slope_intercept(x1, y1, x2, y2){
 	if(x2===x1){
 		console.log('x = '+x1)
 		return;
@@ -13,7 +14,41 @@ function print_equation(x1, y1, x2, y2){
 	console.log('y='+y);*/
 	console.log('y='+m+'x+'+b)
 	
-}              //x1,y1,x2,y2
+};
+
+function slope(x1,y1,x2,y2){
+	var xv=x2-x1;
+	var yv=y2-y1;
+	console.log("Slope="+yv/xv);
+	console.log("Slope fraction="+yv+"/"+xv);
+
+
+};
+var SorSI = readline.question("Slope intercept or just slope? ");
+if(SorSI==="slope intercept" || SorSI==="si" || SorSI==="Slope intercept" || SorSI==="Slope Intercept" || SorSI==="SI"){
+             //x1,y1,x2,y2
+var x1 = readline.question("Type the x1: ");
+var y1 = readline.question("Type the y1: ");
+var x2 = readline.question("Type the x2: ");
+var y2 = readline.question("Type the y2: ");
  //enter the x1,y1 V V V V x2,y2
-	print_equation(1,2,3,4);
+	slope_intercept(x1,y1,x2,y2);
+	
 //example (1,2),(3,4)
+return;
+}
+
+if(SorSI==="slope" || SorSI==="s" || SorSI==="Slope" || SorSI==="S"){
+var x1 = readline.question("Type the x1: ");
+var y1 = readline.question("Type the y1: ");
+var x2 = readline.question("Type the x2: ");
+var y2 = readline.question("Type the y2: ");
+ //enter the x1,y1 V V V V x2,y2
+	slope(x1,y1,x2,y2);
+	return;
+	}
+	
+	else{
+		console.log("No such program or incorrect spelling");
+		return;
+	}
